b11ed409e397fb61c8ab556a45faa763728f0dc4
N3cro0o <calko.n3cro0o@gmail.com>
2025-03-18 22:13:17 +0100

    Philosopher V1

---
+++ DPP/Main.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <random>
+#include <mutex>
+#include <chrono>
+
+// Const
+const int threads = 5;
+
+// State enum
+enum philoState {
+	Sleep,
+	LookForSticks,
+	Eat,
+	PutDownSticks
+};
+
+// Global variables
+std::vector<std::thread> thread_vec;
+std::mutex sticks[threads];
+std::mutex co_mutex;
+
+std::random_device rd;
+std::mt19937 gen;
+std::uniform_int_distribution<> sleep_dist(0, 10);
+std::uniform_int_distribution<> eat_dist(0, 5);
+
+// Control variables
+std::vector<unsigned long long> stick_bounce;
+
+// State functions
+void sleep(philoState* state, int id) {
+	int t = 10 + sleep_dist(gen);
+	co_mutex.lock();
+	std::cout << id << " - Sleep for " << t<< std::endl;
+	co_mutex.unlock();
+	std::this_thread::sleep_for(std::chrono::seconds(t));
+	*state = philoState::LookForSticks;
+}
+
+void getSticks(philoState* state, int id) {
+	while (*state == philoState::LookForSticks) {
+		int left = id;
+		int right = (id + 1) % threads;
+		bool checkL = false;
+		bool checkR = false;
+
+		checkL = !sticks[left].try_lock();
+		checkR = !sticks[right].try_lock();
+
+		if (!checkR && !checkL) {
+			*state = philoState::Eat;
+			int t = 15 + eat_dist(gen);
+			co_mutex.lock();
+			std::cout << id << " - Eat for " << t << std::endl;
+			co_mutex.unlock();
+			std::this_thread::sleep_for(std::chrono::seconds(t));
+		}
+		else {
+			if (!checkL) {
+				sticks[left].unlock();
+			}
+			if (!checkR) {
+				sticks[right].unlock();
+			}
+			stick_bounce[id]++;
+			co_mutex.lock();
+			std::cout << id << " - Bounce\n";
+			co_mutex.unlock();
+			std::this_thread::sleep_for(std::chrono::seconds(3));
+		}
+	}
+}
+
+void dropSticks(philoState* state, int id) {
+	*state = philoState::PutDownSticks;
+	int left = id;
+	int right = (id + 1) % threads;
+	sticks[left].unlock();
+	sticks[right].unlock();
+}
+
+
+// The main functions
+void philosopherLoop(const int id) {
+	srand(time(NULL));
+	philoState state = philoState::Sleep;
+	while (1)
+	{
+		sleep(&state, id);
+		getSticks(&state, id);
+		dropSticks(&state, id);
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	gen = std::mt19937(rd());
+	std::cout << "Philosophers: " << threads << std::endl;
+
+	// Start threads
+	thread_vec.resize(threads);
+	stick_bounce.resize(threads);
+
+	int i = 0;
+	for (std::thread& thr : thread_vec) {
+		thr = std::thread(philosopherLoop, i++);
+	}
+	for (std::thread& thr : thread_vec) {
+		thr.join();
+	}
+
+
+	return 0;
+}
